9c80c5a46e1cb4a8064c6dbac10a39ef719103ec
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-03-06 17:39:51 -0300

    Update FormClientes.cs

--- ProjetoTesteLuiz/ProjetoTesteLuiz/FormClientes.cs
+++ ProjetoTesteLuiz/ProjetoTesteLuiz/FormClientes.cs
@@ -12,59 +12,16 @@ namespace ProjetoTesteLuiz
         {
             InitializeComponent();
             dbContext = new FormDbContext(@"Data Source=LUIZ-DESKTOP\SQLEXPRESS;Initial Catalog=testeLuiz;Integrated Security=False;User ID=usrluiz;Password=1234;");
-        }
-
-        private void desabilitarCampos()
-        {
-            txtNome.Enabled = false;
-            lstSexo.Enabled = false;
-            mskTelefone.Enabled = false;
-            mskCPF.Enabled = false;
-            mskDataNascimento.Enabled = false;
-            lstNacionalidade.Enabled = false;
-            cmdSalvar.Enabled = false;
-            cmdDeletar.Enabled = false;
-            cmdLimpar.Enabled = false;
-            cmdNovo.Enabled = true;
-        }
-
-        private void habilitarCampos()
-        {
-            txtNome.Enabled = true;
-            lstSexo.Enabled = true;
-            mskTelefone.Enabled = true;
-            mskCPF.Enabled = true;
-            mskDataNascimento.Enabled = true;
-            lstNacionalidade.Enabled = true;
-            cmdSalvar.Enabled = true;
-            cmdDeletar.Enabled = true;
-            cmdLimpar.Enabled = true;
-            cmdNovo.Enabled = false;
-        }
-
-        private void limparCampos()
-        {
-            txtNome.Text = "";
-            lstSexo.Text = "";
-            lstNacionalidade.Text = "";
-            mskTelefone.Text = "";
-            mskCPF.Text = "";
-            mskDataNascimento.Text = "";
-            grClientes.DataSource = null;
-            mskCPFPesquisar.Text = "";
-            txtNomePesquisar.Text = "";
-            txtID.Text = "";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
         }
 
         private void AtualizarGrid()
         {
             try
             {
-                // Consulta o banco de dados para obter todos os clientes
                 string sql = "SELECT * FROM vwClientes";
                 DataTable dt = dbContext.ExecuteQuery(sql);
 
-                // Atualiza o DataSource do grid com os novos dados
                 grClientes.DataSource = dt;
             }
             catch (Exception ex)
@@ -96,13 +53,10 @@ namespace ProjetoTesteLuiz
 
         private void grClientes_SelectionChanged(object sender, EventArgs e)
         {
-            // Verifica se há uma linha selecionada no grid
             if (grClientes.SelectedRows.Count > 0)
             {
-                // Obtém o ID do cliente selecionado na grade
                 string clienteID = grClientes.SelectedRows[0].Cells["ID"].Value.ToString();
 
-                // Consulta o banco de dados para obter os detalhes do cliente com base no ID
                 string sql = @"SELECT c.*, n.Nacionalidade
                                FROM tblClientes c
                                INNER JOIN tblNacionalidades n ON c.Nacionalidade_id = n.Id
@@ -113,25 +67,21 @@ namespace ProjetoTesteLuiz
                 {
                     DataTable dt = dbContext.ExecuteQuery(sql, new SqlParameter[] { parameter });
 
-                    // Verifica se foram encontrados dados para o cliente com o ID fornecido
                     if (dt.Rows.Count > 0)
                     {
                         DataRow row = dt.Rows[0];
                         habilitarCampos();
 
-                        // Preenche os campos de entrada com os detalhes do cliente
                         txtID.Text = row["ID"].ToString();
                         txtNome.Text = row["Nome"].ToString();
                         mskCPF.Text = row["CPF"].ToString();
                         mskTelefone.Text = row["Telefone"].ToString();
 
-                        // Verifica se o valor do sexo no banco de dados é NULL
                         string sexo = row["Sexo"] != DBNull.Value.ToString() ? row["Sexo"].ToString() : "\"\"";
                         lstSexo.SelectedItem = sexo;
 
                         lstNacionalidade.Text = row["Nacionalidade"].ToString();
 
-                        // Converte a data de nascimento para o formato esperado e a define no campo correspondente
                         DateTime dataNascimento = Convert.ToDateTime(row["DataNascimento"]);
                         mskDataNascimento.Text = dataNascimento.ToString("ddMMyyyy");
                     }
@@ -173,16 +123,20 @@ namespace ProjetoTesteLuiz
                 string sql;
                 List<SqlParameter> parameters = new List<SqlParameter>();
 
+                if (txtNome.Text.Any(char.IsDigit))
+                {
+                    MessageBox.Show("O campo de nome não pode conter números.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
                 if (!string.IsNullOrEmpty(txtID.Text))
                 {
-                    // Atualização de um cliente existente
                     sql = "UPDATE tblClientes SET Nome = @nome, CPF = @cpf, Telefone = @telefone, Sexo = @sexo, " +
                         "Nacionalidade_id = @nacionalidade, DataNascimento = @dataNascimento WHERE ID = @id";
                     parameters.Add(new SqlParameter("@id", txtID.Text));
                 }
                 else
                 {
-                    // Inserção de um novo cliente
                     sql = "INSERT INTO tblClientes (Nome, CPF, Telefone, Sexo, Nacionalidade_id, DataNascimento, DataHoraCadastro) " +
                         "VALUES (@nome, @cpf, @telefone, @sexo, @nacionalidade, @dataNascimento, @dataHoraCadastro)";
                 }
@@ -191,27 +145,21 @@ namespace ProjetoTesteLuiz
                 parameters.Add(new SqlParameter("@cpf", mskCPF.Text));
                 parameters.Add(new SqlParameter("@telefone", mskTelefone.Text));
 
-                // Verifica se o valor selecionado para Sexo é uma string vazia e, se for, define o valor como null
                 string sexo = lstSexo.Text == "\"\"" ? DBNull.Value.ToString() : lstSexo.Text;
                 parameters.Add(new SqlParameter("@sexo", sexo));
 
                 if (lstNacionalidade.SelectedItem != null)
                 {
                     string nacionalidadeSelecionada = lstNacionalidade.SelectedItem.ToString();
-                    short nacionalidadeId = ObterIdNacionalidade(nacionalidadeSelecionada); // Função para obter o ID com base no nome da nacionalidade
+                    short nacionalidadeId = ObterIdNacionalidade(nacionalidadeSelecionada);
                     parameters.Add(new SqlParameter("@nacionalidade", nacionalidadeId));
                 }
 
-                // Converter a string de data de nascimento para um objeto DateTime
                 DateTime dataNascimento;
-                if (DateTime.TryParseExact(mskDataNascimento.Text, "ddMMyyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataNascimento))
-                {
+                if (ValidarDataNascimento() && DateTime.TryParseExact(mskDataNascimento.Text, "ddMMyyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataNascimento))
                     parameters.Add(new SqlParameter("@dataNascimento", dataNascimento.ToString("yyyy-MM-dd")));
-                }
                 else
-                {
                     throw new ArgumentException("Formato de data de nascimento inválido. Use o formato dd/MM/yyyy. Valor recebido: " + mskDataNascimento.Text);
-                }
 
                 parameters.Add(new SqlParameter("@dataHoraCadastro", DateTime.Now));
 
@@ -268,6 +216,14 @@ namespace ProjetoTesteLuiz
         {
             try
             {
+                string cpf = mskCPF.Text;
+                if (!ValidarCPF(cpf) && cpf != "")
+                {
+                    MessageBox.Show("CPF inválido. Digite um número e CPF válido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    mskCPF.Focus();
+                    mskCPF.Clear();
+                }
+
                 string sql = "SELECT * FROM tblClientes WHERE CPF = @cpf";
                 SqlParameter[] parameters =
                 {
@@ -278,15 +234,12 @@ namespace ProjetoTesteLuiz
                 if (dt.Rows.Count > 0)
                 {
                     DataRow row = dt.Rows[0];
-                    // Se o CPF já existir no banco, traz os dados para a tela
                     txtNome.Text = row["Nome"].ToString();
                     mskTelefone.Text = row["Telefone"].ToString();
                     lstSexo.Text = row["Sexo"].ToString();
                     lstNacionalidade.Text = row["Nacionalidade_id"].ToString();
-                    // Converte a data de nascimento para o formato dd/MM/yyyy antes de atribuí-la ao campo
                     DateTime dataNascimento = Convert.ToDateTime(row["DataNascimento"]);
                     mskDataNascimento.Text = dataNascimento.ToString("ddMMyyyy");
-                    // Defina outras atribuições de campos conforme necessário
 
                     MessageBox.Show("CPF já cadastrado. Os dados foram carregados.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
@@ -295,6 +248,48 @@ namespace ProjetoTesteLuiz
             {
                 MessageBox.Show("Erro ao pesquisar cliente por CPF: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void desabilitarCampos()
+        {
+            txtNome.Enabled = false;
+            lstSexo.Enabled = false;
+            mskTelefone.Enabled = false;
+            mskCPF.Enabled = false;
+            mskDataNascimento.Enabled = false;
+            lstNacionalidade.Enabled = false;
+            cmdSalvar.Enabled = false;
+            cmdDeletar.Enabled = false;
+            cmdLimpar.Enabled = false;
+            cmdNovo.Enabled = true;
+        }
+
+        private void habilitarCampos()
+        {
+            txtNome.Enabled = true;
+            lstSexo.Enabled = true;
+            mskTelefone.Enabled = true;
+            mskCPF.Enabled = true;
+            mskDataNascimento.Enabled = true;
+            lstNacionalidade.Enabled = true;
+            cmdSalvar.Enabled = true;
+            cmdDeletar.Enabled = true;
+            cmdLimpar.Enabled = true;
+            cmdNovo.Enabled = false;
+        }
+
+        private void limparCampos()
+        {
+            txtNome.Text = "";
+            lstSexo.Text = "";
+            lstNacionalidade.Text = "";
+            mskTelefone.Text = "";
+            mskCPF.Text = "";
+            mskDataNascimento.Text = "";
+            grClientes.DataSource = null;
+            mskCPFPesquisar.Text = "";
+            txtNomePesquisar.Text = "";
+            txtID.Text = "";
         }
 
         private void cmdDeletar_Click(object sender, EventArgs e)
@@ -327,13 +322,77 @@ namespace ProjetoTesteLuiz
                 string sql = "SELECT Id FROM tblNacionalidades WHERE Nacionalidade = @nacionalidade";
                 SqlParameter parameter = new SqlParameter("@nacionalidade", nacionalidade);
                 object result = dbContext.ExecuteScalar(sql, parameter);
-                return result != null ? (short)result : (short)0; // Retornar o ID da nacionalidade ou 0 se não for encontrado
+                return result != null ? (short)result : (short)0;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Erro ao obter o ID da nacionalidade: " + ex.Message);
-                return 0; // Retorna 0 em caso de erro
+                return 0;
             }
+        }
+
+        private bool ValidarCPF(string cpf)
+        {
+            if (cpf.Length != 11)
+                return false;
+
+            bool todosIguais = true;
+            for (int i = 1; i < cpf.Length; i++)
+            {
+                if (cpf[i] != cpf[i - 1])
+                {
+                    todosIguais = false;
+                    break;
+                }
+            }
+            if (todosIguais)
+                return false;
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+            }
+            int resto = soma % 11;
+            int digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
+
+            if (digitoVerificador1 != int.Parse(cpf[9].ToString()))
+                return false;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+            }
+            resto = soma % 11;
+            int digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
+
+            if (digitoVerificador2 != int.Parse(cpf[10].ToString()))
+                return false;
+
+            return true;
+        }
+
+        private bool ValidarDataNascimento()
+        {
+            DateTime dataNascimento;
+            if (!DateTime.TryParseExact(mskDataNascimento.Text, "ddMMyyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataNascimento))
+            {
+                return false;
+            }
+
+            if (dataNascimento > DateTime.Now)
+            {
+                return false;
+            }
+
+            if (dataNascimento.Day > 31 || dataNascimento.Month > 12)
+            {
+                return false;
+            }
+
+            mskDataNascimento.Focus();
+            return true;
         }
     }
 }
